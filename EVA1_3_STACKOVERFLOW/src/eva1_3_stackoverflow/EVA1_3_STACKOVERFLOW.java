bf09e47f32d466a8b3f973a2b44a1143efd8f5cf
PabloPizarro8 <ppizarro.c32@hotmail.com>
2021-10-24 10:36:35 -0600

    Create EVA1_3_STACKOVERFLOW.java

---
+++ EVA1_3_STACKOVERFLOW/src/eva1_3_stackoverflow/EVA1_3_STACKOVERFLOW.java
@@ -0,0 +1,18 @@
+
+package eva1_3_stackoverflow;
+
+public class EVA1_3_STACKOVERFLOW {
+
+    public static void main(String[] args) {
+        A();
+    }
+    
+    public static void A(){
+        B();
+    }
+    
+    public static void B(){
+        A();
+    }
+    
+}
