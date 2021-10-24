64187b62c20a5e21ee58bdbd180460e92186db04
PabloPizarro8 <ppizarro.c32@hotmail.com>
2021-10-24 11:07:32 -0600

    Create EVA1_17_EJEMPLO_MATRIZ.java

---
+++ EVA1_17_EJEMPLO_MATRIZ/src/eva1_17_ejemplo_matriz/EVA1_17_EJEMPLO_MATRIZ.java
@@ -0,0 +1,20 @@
+
+package eva1_17_ejemplo_matriz;
+
+public class EVA1_17_EJEMPLO_MATRIZ {
+    
+    public static void main(String[] args) {
+        
+        // Numero de filas
+        int[][] aiMatriz = new int[3][];
+        
+        // Numero de columnas
+        aiMatriz[0] = new int[2];
+        aiMatriz[1] = new int[10];
+        aiMatriz[2] = new int[5];
+        
+        // Cuantos enteros puedo almacenar 
+        
+    }
+    
+}
