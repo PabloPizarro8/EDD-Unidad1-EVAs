a73750f3af4e48935a513f5cbbaf60ca21c7f6f4
PabloPizarro8 <ppizarro.c32@hotmail.com>
2021-10-24 10:26:48 -0600

    algo

---
+++ EVA1_1_TIPOS_DATOS_PRMITIVOS/src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+        // Le daremos el valor maximo posible que un int puede soportar
+        int iVal = Integer.MAX_VALUE;
+        System.out.println("Valor maximo de un integer: " + iVal); // Imprimimos
+        
+        // Agregamos 1 al valor anterior
+        iVal += 1;
+        System.out.println("Lo que ocurre cuando le sumas 1: " + iVal); // Imrpimimos
+        
+        // Le damos el valor minimo posible que un integer puede soportar
+        iVal = Integer.MIN_VALUE;
+        System.out.println("El valor minimo de un integer: " + iVal); // Imprimimos
+        
+        /*
+        for (int i = 0; i < 10; i++){
+            
+            System.out.print("Valor de la i: " + i);
+            
+        }
+        
+        System.out.println("Valor: " + i);
+        */
+        
+    }
+    
+}
