2d0fb9d59827f09586557304666d3b4061bb25d6
PabloPizarro8 <ppizarro.c32@hotmail.com>
2021-10-24 10:51:29 -0600

    Create EVA1_8_ARREGLOS.java

---
+++ EVA1_8_ARREGLOS/src/eva1_8_arreglos/EVA1_8_ARREGLOS.java
@@ -0,0 +1,20 @@
+
+package eva1_8_arreglos;
+
+public class EVA1_8_ARREGLOS {
+    
+    public static void main(String[] args) {
+        
+        int[] aiMiArreglo; // Declaracion, el arreglo no existe todavia
+        // ARREGLOS SON OBJETOS:
+        aiMiArreglo = new int[10]; // Asignando memoria para 10 enteros
+        
+        int[] aiOtroArreglo = new int[50]; // Declaramos y creamos el arreglo
+        
+        double[] adArre1, adArre2, adArre3; // Declaracion de tres arreglos, tipo double
+        
+        double adOtroArre[], dVariable, dOtraVar; // Solo un arreglo, dos variables
+        
+    }
+    
+}
